95434ad9939c1601a42377569037073cb3321b34
Victor Araujo <victor@lunenetworks.com>
2020-03-20 11:10:42 -0700

    added in opportunity grid from master

--- src/components/OpportunityGrid/SeattleGrid.js
+++ src/components/OpportunityGrid/SeattleGrid.js
@@ -48,18 +48,19 @@ class SeattleGrid extends React.Component {
       <div className={classes.gridContainer}>
         <Grid container spacing={24}>
           <Grid item xs>
-            <div className={classes.linkBoxContainer}
+            <div
+              className={classes.linkBoxContainer}
               onClick={() => {
                 this.props.history.push('/Seattle/SHIFA');
               }}
             >
-                <LinkBox
-                  title="SHIFA"
-                  name="STUDENT HEALTH INITIATIVE FOR ACCESS"
-                  description={overviews.SHIFA.Description}
-                  imageLocation="images/Seattle/SHIFA/SHIFA.jpg"
-                  path="Seattle/SHIFA"
-                />              
+              <LinkBox
+                title="SHIFA"
+                name="STUDENT HEALTH INITIATIVE FOR ACCESS"
+                description={this.props.overviews.SHIFA.description}
+                imageLocation={this.props.images['SHIFA.jpg']}
+                path="/Seattle/SHIFA"
+              />
             </div>
           </Grid>
           <Grid item xs>
@@ -72,9 +73,9 @@ class SeattleGrid extends React.Component {
               <LinkBox
                 title="CHAP"
                 name="COMMUNITY HEALTH ADVANCEMENT PROGRAM"
-                description={overviews.CHAP.Description}
-                imageLocation="images/Seattle/CHAP/CHAP.jpg"
-                path="Seattle/CHAP"
+                description={this.props.overviews.CHAP.description}
+                imageLocation={this.props.images['CHAP.jpg']}
+                path="/Seattle/CHAP"
               />
             </div>
           </Grid>
@@ -88,9 +89,9 @@ class SeattleGrid extends React.Component {
               <LinkBox
                 title="UDSM"
                 name="UNIVERSITY DISTRICT STREET MEDICINE"
-                description={overviews.UDSM.Description}
-                imageLocation="images/Seattle/UDSM/UDSM.jpg"
-                path="Seattle/UDSM"
+                description={this.props.overviews.UDSM.description}
+                imageLocation={this.props.images['UDSM.jpg']}
+                path="/Seattle/UDSM"
               />
             </div>
           </Grid>
@@ -104,9 +105,9 @@ class SeattleGrid extends React.Component {
               <LinkBox
                 title="Doc for a Day"
                 name="DFAD"
-                description={overviews.DFAD.Description}
-                imageLocation="images/Seattle/DFAD/DFAD.jpg"
-                path="Seattle/DFAD"
+                description={this.props.overviews.DFAD.description}
+                imageLocation={this.props.images['DFAD.jpg']}
+                path="/Seattle/DFAD"
               />
             </div>
           </Grid>
@@ -120,9 +121,9 @@ class SeattleGrid extends React.Component {
               <LinkBox
                 title="Mobile Health Outreach Van"
                 name=""
-                description={overviews.UMOV.Description}
-                imageLocation="images/Seattle/UMOV/UMOV.jpg"
-                path="Seattle/UMOV"
+                description={this.props.overviews.UMOV.description}
+                imageLocation={this.props.images['UMOV.jpg']}
+                path="/Seattle/UMOV"
               />
             </div>
           </Grid>
@@ -136,19 +137,29 @@ class SeattleGrid extends React.Component {
               <LinkBox
                 title="UTest"
                 name="Rapid HIV Testing"
-                description={overviews.UTEST.Description}
-                imageLocation="images/Seattle/UTEST/UTEST.jpg"
-                path="Seattle/UTEST"
+                description={this.props.overviews.UTEST.description}
+                imageLocation={this.props.images['UTEST.jpg']}
+                path="/Seattle/UTEST"
               />
             </div>
           </Grid>
           <Grid item xs>
-            <Link to={{pathname: 'Seattle/Others'}} className={classes.otherProjectsLink}>
-              <div className={classes.otherProjectsDiv}
-              onClick={() => { this.props.history.push('/Seattle/Others') }}>
-                <Typography variant='h4' className={classes.otherProjectsText}>
-                    All other UWSOM Service Learning Projects
-                </Typography>      
+            <Link
+              to={{ pathname: '/Seattle/Others' }}
+              className={classes.otherProjectsLink}
+            >
+              <div
+                className={classes.otherProjectsDiv}
+                onClick={() => {
+                  this.props.history.push('/Seattle/Others');
+                }}
+              >
+                <Typography
+                  variant="h4"
+                  className={classes.otherProjectsText}
+                >
+                  All other UWSOM Service Learning Projects
+                </Typography>
               </div>
             </Link>
           </Grid>
