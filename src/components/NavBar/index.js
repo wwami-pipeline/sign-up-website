1e8ebbeff2d1222256f26e978313cf75422c91a6
Victor Araujo <victor@lunenetworks.com>
2020-05-20 08:21:43 -0700

    fixed navbar using React Navbar dependency

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,40 +1,31 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-import {
-    AppBar,
-    Tab,
-    Tabs,
-    Typography
-  } from "@material-ui/core";
-
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
 import './index.css';
 
 const NavBar = () => {
     return(
       <div className="App">
-            <Fragment>
-                <AppBar position="relative">
-                    <div>
-                      <div style={{float:'left'}}>
-                        <img className="toolbar-logo" alt="UW School of Medicine Volunteer Logo" src="/images/uw_name_logo.jpg" />
-                        <br/>
-                        <Typography style={{color:'#440088', fontWeight: 'bold', margin: '0px 0px 0px 20px'}} variant="h5">
-                          SCHOOL OF MEDICINE SERVICE LEARNING
-                        </Typography>
-                      </div>
-                      <div style={{display:'inline'}}>
-                        <img className="wwami-logo" alt="UW School of Medicine Volunteer Logo" src="/images/wwami_logo.png" />
-                      </div>
-                    </div>
-                    <Tabs>
-                        <Tab label={<span className="tabLabel">Home</span>} component={Link} to="/" />
-                        <Tab label={<span className="tabLabel">Resources</span>} component={Link} to="/resources" />
-                        <Tab label={<span className="tabLabel">About Us</span>} component={Link} to="/about" />
-                        <Tab label={<span className="tabLabel">Contact Us</span>} component={Link} to="/contact" />
-                        <Tab label={<span className="tabLabel">Donate</span>} component={Link} to="/donate" />
-                    </Tabs>
-              </AppBar>
-            </Fragment>
+        <div style={{ backgroundColor: "#f8f9fa" }}>
+          <a href="/" >
+            <img className="toolbar-logo" alt="UW School of Medicine Volunteer Logo" src="/images/logo2.jpg" />
+          </a>
+            <br/>
+        </div>
+        <Navbar bg="light" expand="lg">
+          <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link className="tabLabel" href="/">Home</Nav.Link>
+                <Nav.Link className="tabLabel" href="/about">About</Nav.Link>
+                <Nav.Link className="tabLabel" href="/resources">Resources</Nav.Link>
+                <Nav.Link className="tabLabel" href="/donate">Donations</Nav.Link>
+                <Nav.Link className="tabLabel" href="/contact">Contact Us</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Navbar>
       </div>
     )
 }
