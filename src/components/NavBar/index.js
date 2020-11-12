0d0b5419de0600146928d878e6b536d6f3e2363d
Zizhen Song <zizhensong@gmail.com>
2020-11-11 21:25:55 -0800

    added nav router support

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,38 +2,69 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from "react-router-bootstrap";
 import './index.css';
 
 const NavBar = () => {
     return(
       <div className="App">
         <Navbar collapseOnSelect bg="light" expand="lg">
-          <Navbar.Brand href="/">
-            <img className="toolbar-logo" alt="UW School of Medicine Volunteer Logo" src="/images/uw_name_logo.jpg" />
-          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img className="toolbar-logo" alt="UW School of Medicine Volunteer Logo" src="/images/uw_name_logo.jpg" />
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link className="tabLabel" href="/">Home</Nav.Link>
+              <LinkContainer to="/">
+                <Nav.Link className="tabLabel">Home</Nav.Link>
+              </LinkContainer>
               <NavDropdown className="tabLabel" title="About" id="collasible-nav-dropdown">
-                <NavDropdown.Item className="tabSubLabel" href="/about"> About Us </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/contact"> Contact </NavDropdown.Item>
+                <LinkContainer to="/about">
+                  <NavDropdown.Item className="tabSubLabel"> About Us </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/contact">
+                  <NavDropdown.Item className="tabSubLabel"> Contact </NavDropdown.Item>
+                </LinkContainer>
               </NavDropdown>
               <NavDropdown className="tabLabel" title="Resources" id="collasible-nav-dropdown">
-                <NavDropdown.Item className="tabSubLabel" href="/resources/links"> Website Links </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/resources/videos"> Training Videos </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/resources/tools"> Teaching Tools </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/resources/protocols"> Protocols </NavDropdown.Item>
+                <LinkContainer to="/resources/links">
+                  <NavDropdown.Item className="tabSubLabel"> Website Links </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/resources/videos">
+                  <NavDropdown.Item className="tabSubLabel"> Training Videos </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/resources/tools">
+                  <NavDropdown.Item className="tabSubLabel"> Teaching Tools </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/resources/protocols">
+                  <NavDropdown.Item className="tabSubLabel"> Protocols </NavDropdown.Item>
+                </LinkContainer>
               </NavDropdown>
               <NavDropdown className="tabLabel" title="Other Student Orgs" id="collasible-nav-dropdown">
-                <NavDropdown.Item className="tabSubLabel" href="/outsideOrganizations/Seattle"> Seattle </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/outsideOrganizations/Spokane"> Spokane </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/outsideOrganizations/Wyoming"> Wyoming </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/outsideOrganizations/Alaska"> Alaska </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/outsideOrganizations/Montana"> Montana </NavDropdown.Item>
-                <NavDropdown.Item className="tabSubLabel" href="/outsideOrganizations/Idaho"> Idaho </NavDropdown.Item>
+                <LinkContainer to="/outsideOrganizations/Seattle">
+                  <NavDropdown.Item className="tabSubLabel"> Seattle </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/outsideOrganizations/Spokane">
+                  <NavDropdown.Item className="tabSubLabel"> Spokane </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/outsideOrganizations/Wyoming">
+                  <NavDropdown.Item className="tabSubLabel"> Wyoming </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/outsideOrganizations/Alaska">
+                  <NavDropdown.Item className="tabSubLabel"> Alaska </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/outsideOrganizations/Montana">
+                  <NavDropdown.Item className="tabSubLabel"> Montana </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/outsideOrganizations/Idaho">
+                  <NavDropdown.Item className="tabSubLabel"> Idaho </NavDropdown.Item>
+                </LinkContainer>
               </NavDropdown>
-              <Nav.Link className="tabLabel" href="/donate">Donations</Nav.Link>
+              <LinkContainer to="/donate">
+                <Nav.Link className="tabLabel">Donations</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
